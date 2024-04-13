c348621a1ededfc33d32ae7aa0bb5f61671f0fa1
leobarbosadev <leobarbosajf@hotmail.com>
2024-04-12 21:20:54 -0300

    Hello World!!

---
+++ aula001_primeiro_programa/aula001.py
@@ -0,0 +1,19 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Leonardo Barbosa
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+#Importando as bibliotecas
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print() executa uma saída via terminal
+
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
