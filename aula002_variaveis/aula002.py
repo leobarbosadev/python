effa2bf1d740d4bc28805798a8b25d98caebbba2
leobarbosadev <leobarbosajf@hotmail.com>
2024-04-16 19:06:01 -0300

    editado exercicio 2 e criado pasta aula 3

--- aula002_variaveis/aula002.py
+++ aula002_variaveis/aula002.py
@@ -4,28 +4,28 @@
 # Data: 15/04/2024
 # Criando variáveis
 
-#importando a biblioteca de sistema
+# importando a biblioteca de sistema
 import os
 
 
-#Limpando o terminal
+# Limpando o terminal
 os.system('cls')
 
-print('-' * 70) #firula
+print('-'*70)  # firula
 print('Estudo de variáveis')
-print('-' * 70)#firula
+print('=' * 70)  # firula
 
-#As variáveis são declaradas por inferência
+# As variáveis são declaradas por inferência
 nome = 'John Doe'
 nascimento = 1970
 altura = 1.80
 peso = 75.5
 doador = True
-complexo = 3j #Python trabalha diretamente com números complexos
-PI = 3.14 # isso é uma CONSTANTE, seu valor não deve ser alterado
+complexo = 3j  # Python trabalha diretamente com números complexos
+PI = 3.14  # isso é uma CONSTANTE, seu valor não deve ser alterado
 
-#Saída utilizando o método type() para verificar o
-#tipo da variável
+# Saída utilizando o método type() para verificar o
+# tipo da variável
 print('\033[0m \033[31m Tipos declarados:\033[0m')
 print('\033[0m A var \033[32m nome \033[0m é do tipo: ', type(nome))
 print('\033[0m A var \033[32m nascimento \033[0m é tipo: ', type(nascimento))
@@ -36,5 +36,3 @@ print('\033[0m A var \033[32m complexo \033[0m é do tipo:', type(complexo))
 print('\033[0m A var \033[32m PI \033[0m é do tipo:', type(PI))
 print('-' * 70)
 print('')
-
-
