6eab40ff6a7920d7bc77db17937ed4824edd13c7
leobarbosadev <leobarbosajf@hotmail.com>
2024-04-29 19:02:56 -0300

    Atualização exercício h

--- atividade002/h.py
+++ atividade002/h.py
@@ -2,7 +2,11 @@
 # Turma 0152
 # Autor: Leonardo Barbosa
 # Data: 29/04/2024
-#H) A empresa "RootCalc" está desenvolvendo um software de cálculo de raízes de equações quadráticas para auxiliar engenheiros e cientistas em suas análises e projetos. Eles precisam de um programa que calcule as raízes da equação quadrática 𝑥²−6𝑥+5 sem depender de funções ou métodos predefinidos, utilizando apenas os conceitos e operações básicas aprendidos até o momento. Essas raízes são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser capaz de calcular essas raízes de forma precisa, seguindo os princípios matemáticos fundamentais.
+#H) A empresa "RootCalc" está desenvolvendo um software de cálculo de raízes de equações quadráticas para auxiliar 
+# engenheiros e cientistas em suas análises e projetos. Eles precisam de um programa que calcule as raízes da equação 
+# quadrática 𝑥²−6𝑥+5 sem depender de funções ou métodos predefinidos, utilizando apenas os conceitos e operações 
+# básicas aprendidos até o momento. Essas raízes são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser 
+# capaz de calcular essas raízes de forma precisa, seguindo os princípios matemáticos fundamentais.
 
 import os
 a = 1
