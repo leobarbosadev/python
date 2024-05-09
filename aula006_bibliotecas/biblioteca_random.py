32b285b5183e00e0308c0f2c36753a0c4e9ee761
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-09 20:52:14 -0300

    Criaçao da biblioteca rand

---
+++ aula006_bibliotecas/biblioteca_random.py
@@ -0,0 +1,46 @@
+import random
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('BIBLIOTECA RANDOM')
+print('=' * 70)
+
+print('Número aleatório')
+numero_aleatorio = random.random()
+print(f'O número gerado foi: {numero_aleatorio}')
+print('.' * 70)
+
+print('Número aleatório inteiro')
+aleatorio_inteiro = random.randint(1, 20)
+print(f'O número inteiro gerado foi: {aleatorio_inteiro}')
+print('.' * 70)
+
+print('Número aleatório decimal no intervalo')
+aleatorio_decimal = random.uniform(1, 10)
+print(f'O número decimal gerado foi: {aleatorio_decimal}')
+print('.' * 70)
+
+print('Sorteio em um lista')
+lista = ['Ágata', 'Coly', 'Isis', 'Bia']
+nome_sorteado = random.choice(lista)
+print(f'Lista: {lista}')
+print(f'O nome escolhido foi: {nome_sorteado}')
+print('.' * 70)
+
+print('Embaralhar sequência')
+lista2 = ['Ágata', 'Coly', 'Isis', 'Bia']
+print(f'Lista antiga: {lista2}')
+# embaralhando = list(random.shuffle(lista)) dá erro
+# embaralhando = random.shuffle(lista2) saída vazia
+random.shuffle(lista2)
+print(f'Lista nova: {lista2}')
+print('.' * 70)
+
+print('Retorno de elementos únicos de uma população')
+numeros = [1, 2, 3, 4, 5, 6, 7]
+amostra_aleatória = random.sample(numeros, 5)
+print(f'Retorno de amostragem: {amostra_aleatória}')
+print('.' * 70)
