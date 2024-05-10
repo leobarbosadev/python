bad598e8c4e2a87ba42d2fcbb3297c4c9401db1c
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-09 21:39:41 -0300

    Atividade a.py

--- atividade003/a.py
+++ atividade003/a.py
@@ -1,11 +1,18 @@
-#Faça um programa que receba um valor e mostre sua raiz quadrada. Para raízes que não são exatas, arredonde para cima ou para baixo. 
+# Faça um programa que receba um valor e mostre sua raiz quadrada. Para raízes que não são exatas, arredonde para cima ou para baixo.
 # Faça a validação para números negativos, avisando ao usuário que o resultado será um número complexo.
 
 import os
 import math
 
-#Entrada
-numrero = int(input('Entre com um número inteiro: '))
 
-#Processamento
-raiz
+os.system('cls')
+
+# Entrada
+numero = int(input('Entre com um número inteiro: '))
+
+# Condicional
+if numero < 0:
+    print('Número negativo, o resultado será um número complexo')
+else:
+    raiz = math.sqrt(numero)        
+    print(f'Raiz quadra de {numero} é {raiz}')
