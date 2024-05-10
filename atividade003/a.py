41d4763c6a00d0a96ec3f3df64a07476e10b6d99
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-09 21:05:30 -0300

    Criação da atividade 003

---
+++ atividade003/a.py
@@ -0,0 +1,11 @@
+#Faça um programa que receba um valor e mostre sua raiz quadrada. Para raízes que não são exatas, arredonde para cima ou para baixo. 
+# Faça a validação para números negativos, avisando ao usuário que o resultado será um número complexo.
+
+import os
+import math
+
+#Entrada
+numrero = int(input('Entre com um número inteiro: '))
+
+#Processamento
+raiz
---
+++ atividade003/d.py
@@ -0,0 +1,4 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Leonardo Barbosa
+# Data: 10/04/2024
