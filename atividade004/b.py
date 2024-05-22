a6241c305eea12cf62de9217357ecc230e42d125
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-21 21:22:04 -0300

    Desenvolvimento do exercício b

---
+++ atividade004/b.py
@@ -0,0 +1,13 @@
+#Faça um programa que receba o nome 'João da Silva' e, em seguida, substitua 'Silva' por 'Oliveira'.
+
+import os
+
+
+os.system('cls')
+
+nome = 'João da Silva'
+
+substituir = nome.replace('Silva','Oliveira')
+print(f'Nome original: {nome}')
+print(f'Nome novo: {substituir}')
+print()
