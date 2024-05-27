40739cf62e93d3a7dda50b7f5c7f85eb29d85b1b
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-27 20:49:07 -0300

    Validação exercício b

--- atividade004/b.py
+++ atividade004/b.py
@@ -1,13 +1,20 @@
-#Faça um programa que receba o nome 'João da Silva' e, em seguida, substitua 'Silva' por 'Oliveira'.
+# Faça um programa que receba o nome 'João da Silva' e, em seguida, substitua 'Silva' por 'Oliveira'.
 
 import os
 
 
 os.system('cls')
 
-nome = 'João da Silva'
+nome = input('Entre com seu nome: ').lower()
 
-substituir = nome.replace('da Silva','de Oliveira')
-print(f'Nome original: {nome}')
-print(f'Nome novo: {substituir}')
-print()
+
+# Validação
+if not (nome.replace(' ', '').isalpha()):
+    print('Contém caracter inválido!!!')
+if 'silva' in nome:
+    # lista = nome.split(' ')
+    substituir = nome.replace('silva', 'Oliveira')
+    print(f'Nome original: {nome.title()}')
+    print(f'Nome novo: {substituir.title()}') # O MÉTODO .title() DEIXA A PRIMEIRA LETRA DE CADA PALAVRA EM MAÍSUCULA
+else:
+    print('Não foi encontrado Silva no seu nome')
