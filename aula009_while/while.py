9057554dbbfd7be99a7e74eef7dca380f9b88637
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-28 21:03:54 -0300

    Criação da pasta While

---
+++ aula009_while/while.py
@@ -0,0 +1,18 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('COMANDO WHILE')
+print('-' * 70)
+
+contador = 1
+
+while contador <= 10:
+    print(f'contando...{contador}')
+
+    # Contador += 1 contador = contador + 1
+    contador += 1
+
+print('Fim do programa!\n')
