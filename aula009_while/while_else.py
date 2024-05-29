fef256d8bcdeb391e5ec087db1de09661424129e
leobarbosadev <leobarbosajf@hotmail.com>
2024-05-28 21:19:45 -0300

    Criação While else

---
+++ aula009_while/while_else.py
@@ -0,0 +1,24 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE WHILE ELSE')
+print('=' * 70)
+print()
+print()
+
+contador = 1
+
+while contador < 7:
+    print(f'Contador é:{contador}') # Posso usar da seguinte forma tambem 'contador é:', contador)
+    contador += 1
+    
+    # Se eu tirar essa condicional o else será executado
+    if contador == 4:
+        print(f'Contador chegou em {contador}. Break no While!')
+        break
+else:
+    print('While finalizado!')
+print()
