da249458a8f4366c82932f0f0bd7fc322ae882e0
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-06-01 14:42:49 -0300

    Atualização teste.py

--- testes/testes.py
+++ testes/testes.py
@@ -35,25 +35,3 @@
 #     else:
 #         print(f'"{numero}" Entrada inválida!')
 #         print()
-
-import os 
-
-
-os.system('cls')
-
-print('SOMATÓRIO DE NÚMEROS PARES')
-print()
-
-a = int(input('Insira o menor número: '))
-b = int(input('Insira o maior número: '))
-print()
-
-c = 0
-soma = 0
-
-for c in range(a, (b + 1), 2):
-    numero = c
-    
-    soma += numero
-    
-    print(soma, end=' | ')
