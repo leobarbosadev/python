fdd7a3aee359383a5ebd9ee32e42e03b4c7d6d0f
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-06-02 22:04:47 -0300

    atualização exercício b

--- atividade004/b.py
+++ atividade004/b.py
@@ -2,18 +2,35 @@
 
 import os
 
-
 os.system('cls')
 
-nome = input('Entre com seu nome: ').lower()
+print('-' * 80)
+print('*** SUBSTITUIÇÃO DO SOBRENOME ***')
+print('=' * 80)
 
-# Validação
-if not (nome.replace(' ', '').isalpha()):
-    print('Contém caracter inválido!!!')
-if 'silva' in nome:
-    # lista = nome.split(' ')
-    substituir = nome.replace('silva', 'Oliveira')
-    print(f'Nome original: {nome.title()}')
-    print(f'Nome novo: {substituir.title()}') # O MÉTODO .title() DEIXA A PRIMEIRA LETRA DE CADA PALAVRA EM MAÍSUCULA
-else:
-    print('Não foi encontrado Silva no seu nome')
+nome = 'João da Silva'
+nome_novo = nome.replace('da Silva', 'de Oliveira')
+
+print(f'Nome original: {nome}')
+print(f'Nome novo: {nome_novo}')
+
+print('-' * 80)
+print()
+
+
+# os.system('cls')
+
+# nome = input('Entre com seu nome: ').lower()
+
+# # Validação
+# if not (nome.replace(' ', '').isalpha()):
+#     print('Contém caracter inválido!!!')
+# if 'silva' in nome:
+#     # lista = nome.split(' ')
+#     substituir = nome.replace('silva', 'Oliveira')
+#     print(f'Nome original: {nome.title()}')
+#     print(f'Nome novo: {substituir.title()}') # O MÉTODO .title() DEIXA A PRIMEIRA LETRA DE CADA PALAVRA EM MAÍSUCULA
+# else:
+#     print('Não foi encontrado Silva no seu nome')
+
+
