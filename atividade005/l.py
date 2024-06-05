ae35271fe09bd4df3aecdd5e67999a723f6dd5d8
leobarbosadev <leobarbosajf@hotmail.com>
2024-06-05 20:46:19 -0300

    Conclusão l

--- atividade005/l.py
+++ atividade005/l.py
@@ -1,2 +1,24 @@
 #Faça um programa que verifique se o usuário e senha estão inseridos em um banco de dados (fake). 
 # O usuário só terá acesso se digitar os dados corretos e, assim, sair do laço.
+
+import os
+
+
+os.system('cls')
+
+print('-' * 80)
+print('USUÁRIO E SENHA')
+print('=' * 80)
+
+
+while True:
+    usuario = input('Entre com o usuário: ').lower()
+    senha = input('Entre com a senha: ')
+    
+    if (usuario == 'leonardo' and senha == '1234'):
+        print('Acesso permitido')
+        print()
+        break
+    else:
+        print('Acesso negado')
+    os.system('cls')
