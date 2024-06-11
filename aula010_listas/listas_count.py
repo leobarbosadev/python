30273bbd2c903ce53f101247edf0d5512ef5a156
Leonardo Barbosa <103702717+leobarbosadev@users.noreply.github.com>
2024-06-11 19:37:47 -0300

    Create listas_count.py

---
+++ aula010_listas/listas_count.py
@@ -0,0 +1,29 @@
+Método count()
+
+
+# import os
+
+
+# os.system('cls')
+
+#Solicita ao usuário para inserir números separados por espaço
+entrada = input('Diigite números separados por espaços: ')
+
+#Divide a string de entrada em uma lista de strings
+numeros_str = entrada.split()
+
+#Converte a lista de strings e uma lista de inteiros
+numeros = []
+for num_str in numeros_str:
+    numeros.append(int(num_str))
+
+#Solicita ao usuário para inserir o número que deseja contar na lista
+numero_para_contar = int(input('Digite o número que deseja contar: '))
+
+#Conta o número de vezes que o número fornecido aparece na lista
+contagem = numeros.count(numero_para_contar)
+
+if contagem > 0:
+    print(f'O número {numero_para_contar} aparece {contagem} vezes na lista.')
+else:
+    print(f'O número {numero_para_contar} não aparece na lista')
