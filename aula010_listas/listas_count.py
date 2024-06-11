22bfe0ba94062d1949560105fa8c8212f435bf97
Leonardo Barbosa <103702717+leobarbosadev@users.noreply.github.com>
2024-06-11 19:50:22 -0300

    Update listas_count.py

--- aula010_listas/listas_count.py
+++ aula010_listas/listas_count.py
@@ -27,3 +27,6 @@ if contagem > 0:
     print(f'O número {numero_para_contar} aparece {contagem} vezes na lista.')
 else:
     print(f'O número {numero_para_contar} não aparece na lista')
+
+#Exibe a lista fornecida para referencia
+print(f'Lista fornecida: {numeros}')
