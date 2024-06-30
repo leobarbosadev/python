9f07f142633a969165e0d2b7eeec5644949c9701
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-06-30 20:53:19 -0300

    Finalização exercicio f

--- atividade009/f.py
+++ atividade009/f.py
@@ -9,27 +9,33 @@ import os
 
 os.system('cls')
 
-print('-' * 70)
-print('DICIONÁRIO DE VINHOS')
-print('=' * 70)
+# print('-' * 70)
+# print('DICIONÁRIO DE VINHOS')
+# print('=' * 70)
 
 vinhos = {}
 lista_vinhos = []
 
 # CONTAGEM
-for c in range(5):
+for c in range(2):
+    print('-' * 70)
+    print('DICIONÁRIO DE VINHOS')
+    print('=' * 70)
     print(f'Entre com o {c + 1}º vinho: ')
     vinhos['nome'] = input('Entre com o nome.........: ')
     vinhos['tipo'] = input('Entre com o tipo.........: ')
     vinhos['teor'] = input('Entre com o teor alcólico: ')
     vinhos['safra'] = input('Entre com a safra........: ')
-    print()
-    
+    os.system('cls')
+
     # USO O append() E O copy() PARA ADICIONAR UMA CÓPIA DO DICIONÁRIO NA LISTA
     lista_vinhos.append(vinhos.copy())
 
-print('Vinhos:')
+print('-' * 70)
+print('LISTA COM TODOS OS VINHOS')
+print('=' * 70)
 for elemento in lista_vinhos:  # PARA CADA ELEMENTO NA LISTA
     for chave, valor in elemento.items():  # PARA CADA CHAVE E VALOR DO DICIONÁRIO
         print(f'{chave.capitalize()}:{valor.capitalize()}', end=' | ')
     print()
+print()
--- atividade009/g.py
+++ atividade009/g.py
@@ -4,3 +4,11 @@
 # Data: 01/07/2024
 #g) Faça um programa para cadastrar os alunos de uma escola. Para os campos,
 # tome como referência o nome do aluno, data de nascimento e matrícula.
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('DICIONÁRIO DE ALUNOS')
+print('=' * 70)
