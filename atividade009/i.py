364c32b7837efe08b80abca2618521b1f1872113
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-06-30 22:30:39 -0300

    finalizado a j, verificar chave:valor

--- atividade009/i.py
+++ atividade009/i.py
@@ -5,3 +5,48 @@
 #i) Faça um programa para criar um dicionário com 4 elementos.
 # Depois imprima a lista completa,
 # delete o último elemento e mostre uma listagem nova.
+import os
+
+
+os.system('cls')
+
+caracteristicas = {}
+lista_caracteristica = []
+quantidade = input('Quantos itens deseja adicionar? ')
+quantidade = int(quantidade)
+
+# CONTAGEM
+for c in range(quantidade):
+    print('-' * 70)
+    print('DICIONÁRIO 4 ELEMENTOS')
+    print('=' * 70)
+    print(f'Adicionando o {c + 1}º elemento')
+    caracteristicas['nome'] = input('Digite o nome..: ')
+    caracteristicas['idade'] = input('Digite a idade.: ')
+    caracteristicas['peso'] = input('Digite o peso..: ')
+    caracteristicas['altura'] = input('Digite a altura: ')
+    os.system('cls')
+    
+    lista_caracteristica.append(caracteristicas.copy())
+    
+print('-' * 70)
+print('LISTA COM TODOS OS ITENS CADASTRADOS')
+print('=' * 70)
+
+for elemento in lista_caracteristica: # PARA CADA ELEMENTO NA LISTA
+    for chave, valor in elemento.items():
+        print(f'{chave.capitalize()}:{valor.capitalize()}', end=' | ')
+    print()
+print()
+
+print('O último elemento será excluído')
+lista_caracteristica.pop()
+
+print('-' * 70)
+print('LISTAGEM COM O ÚLTIMO ELEMENTO EXCLUÍDO')
+print('=' * 70)
+for elemento in lista_caracteristica:
+    for chave, valor in elemento.items():
+        print(f'{chave}: {valor}', end=' | ')
+    print()
+print()
--- atividade009/j.py
+++ atividade009/j.py
@@ -4,3 +4,29 @@
 # Data: 01/07/2024
 #j) Faça um programa para criar um dicionário com nomes de frutas.
 # Em seguida verifique se tem abacaxi nos valores.
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ADICIONANDO FRUTAS')
+print('=' * 70)
+
+fruta = 'Abacaxi'
+frutas = {}
+quantidade_itens = input('Quantos itens deseja adicionar? ')
+print()
+quantidade_itens = int(quantidade_itens)
+
+for c in range(quantidade_itens):
+    print(f'Adicionando o {c + 1}º item:')
+    chave = input('Insira a chave: ')
+    valor = input('Insira o valor: ')
+    frutas[chave] = valor
+    print()
+    
+if fruta in frutas:
+    print(f'{fruta} foi encontrado')
+else:
+    print(f'{fruta} não foi encontrado')
