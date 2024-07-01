c16fbdf78332a8bb1ea414ebc1e34c8d31f64bdb
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-06-30 21:36:41 -0300

    TERMINAR O EXERCICIO h

--- atividade009/h.py
+++ atividade009/h.py
@@ -7,3 +7,16 @@
 # Em seguida realize as seguintes ações:
 # - Listar chaves e valores com laço - Deletar o peso
 # - Listar novamente chaves e valores - mostrar o nome e altura
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('DICIONÁRIO NOME, IDADE, PESO, ALTURA')
+print('=' * 70)
+
+nomes = {'nome': 'John', 'idade': 40, 'peso': 80, 'altura': 1.70}
+
+if nomes:
+    print(f'Itens do dicionário: {nomes.items()}')
