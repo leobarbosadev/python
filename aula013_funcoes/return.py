7b0a9910fdb21f5782c491db5ca31f491f53fc43
leobarbosadev <leobarbosajf@hotmail.com>
2024-07-10 21:33:16 -0300

    Tentando atualizando return (desafio)

--- aula013_funcoes/return.py
+++ aula013_funcoes/return.py
@@ -9,20 +9,32 @@ os.system('cls')
 def calcular_velocidade_media(distancia, tempo):
     # Vm = deltaS/deltat
 
-    if distancia < 1000:
+    # if distancia < 1000:
+    #     divide_distancia = 1
+    # else:
+    #     divide_distancia = distancia / 1000
+
+    # if tempo < 60:
+    #     divide_tempo = 1
+    # else:
+    #     divide_tempo = tempo / 60
+
+
+    # 
+    
+    
+    if distancia < 1000 and tempo <  60:
         divide_distancia = 1
+        divide_tempo = 1
+        print(f'A velociade média é {velocidade:.2f} m/s.')
+    
     else:
         divide_distancia = distancia / 1000
-
-    if tempo < 60:
-        divide_tempo = 1
-    else:
         divide_tempo = tempo / 60
-
-    velocidade_media = (distancia / divide_distancia) / (tempo / divide_tempo)
-
+        print(f'A velociade média é {velocidade:.2f} km/h.')
+        
     return velocidade_media
-
+    velocidade_media = (distancia / divide_distancia) / (tempo / divide_tempo)
 
 distancia = float(input('Dgite a distância percorrida (em KM): '))
 tempo = float(input('Digite o tempo gasto (em horas): '))
