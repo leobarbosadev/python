ef862ad680eb9a47f75cf69b203e2563193eb5bd
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-07-17 22:21:56 -0300

    Conclusão exercício e

--- atividade010/d.py
+++ atividade010/d.py
@@ -10,7 +10,7 @@ import os
 os.system('cls')
 
 
-def converter_graus():
+def converter_graus():  # SE EU NÃO PASSAR A TEMP. TBM FUNCIONA
     temperatura_convertida = (temperatura - 32) * 5/9
 
     return temperatura_convertida
@@ -18,7 +18,6 @@ def converter_graus():
 
 temperatura = float(input('Entre com a temperatura em Fahrenheit: '))
 
-convertendo = converter_graus()
-
+convertendo = converter_graus() # SE EU NÃO PASSAR A TEMP. TBM FUNCIONA
 print(f'A temperatura {temperatura} °F é igual a {convertendo:.2f} °C')
 print()
--- atividade010/e.py
+++ atividade010/e.py
@@ -2,5 +2,23 @@
 # Turma 0152
 # Autor: Leonardo Barbosa
 # Data:
-# Crie uma função que receba uma temperatura
-# em fahrenheit e retorne o valor em graus célsius
+#Crie uma função que receba a altura e o peso de uma pessoa.
+# Depois retorne o seu IMC.
+import os
+
+
+os.system('cls')
+
+def calcular_imc():
+    calculo_imc = peso / (altura * altura) # altura **2
+    
+    return calculo_imc
+    
+    
+altura = float(input('Entre com sua altura (em m): '))
+peso = float(input('Entre com o seu peso (em kg): '))
+
+imc = calcular_imc()
+
+print(f'O seu imc é de {imc:.2f}')
+print()
