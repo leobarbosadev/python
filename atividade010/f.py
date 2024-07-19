b4de68259fb69c540e1118a7aab5d822bdf00677
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-07-18 21:36:12 -0300

    Finalizado atividade f

--- atividade010/f.py
+++ atividade010/f.py
@@ -2,5 +2,27 @@
 # Turma 0152
 # Autor: Leonardo Barbosa
 # Data:
-# Crie uma função que receba uma temperatura
-# em fahrenheit e retorne o valor em graus célsius
+# Crie uma função que receba 2 listas:
+# - lista 1: nome, peso, idade
+# - lista 2: John, 40, 1.8
+# - Sua função deverá criar um dicionário contendo chave e valor utilizando como
+# base essas duas listas. Depois, seu programa deverá imprimir esse dicionário
+# utilizando uma estrutura de repetição for.
+import os
+
+
+os.system('cls')
+
+lista_1 = ['nome', 'peso', 'idade']
+lista_2 = ['John', 40, 1.8]
+
+
+def receber_listas():
+
+    dicionario = dict(zip(lista_1, lista_2))
+
+    for chave, valor in dicionario.items():
+        print(f'{chave}: {valor}')
+
+receber_listas()
+print()
