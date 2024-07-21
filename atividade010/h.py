fdf2dabe3deebfecc12a86e5520ae295098f853a
Leonardo Barbosa <leobarbosajf@hotmail.com>
2024-07-21 12:37:12 -0300

    criação dos enunciados dos exercícios h, i

--- atividade010/h.py
+++ atividade010/h.py
@@ -2,5 +2,9 @@
 # Turma 0152
 # Autor: Leonardo Barbosa
 # Data:
-# Crie uma função que receba uma temperatura
-# em fahrenheit e retorne o valor em graus célsius
+# Uma Academia deseja fazer uma pesquisa entre seus clientes para
+# descobrir a média de altura  e peso de seus clientes.
+# Faça um programa que pergunte a cada um dos clientes da academia seu código
+# nome, altura e peso. Após esse cadastramento, seu programa deverá listar
+# os dados dos clientes e a média. Para sair do programa o usuário
+# deverá digitar o valor zero(0). O número de clientes é indefinido.
--- atividade010/i.py
+++ atividade010/i.py
@@ -2,5 +2,8 @@
 # Turma 0152
 # Autor: Leonardo Barbosa
 # Data:
-# Crie uma função que receba uma temperatura
-# em fahrenheit e retorne o valor em graus célsius
+# Faça um programa de perguntas e respostas sobre os estados e capitais
+# brasileiras. O programa deverá exibir para o usuário o estado e perguntar
+# qual a sua capital. Se o usuário errar a resposta, o programa será finalizado
+# apresentando quantas perguntas estão corretas.
+# Utilize ao menos uma função e não há a necessidade de modularizar o código.
