bd847c284f5306b2b9bb9f2ee3cf5b356147b40e
leobarbosadev <leobarbosajf@hotmail.com>
2024-09-03 19:11:33 -0300

    exercicio c feito

---
+++ OO/atividade001/c.py
@@ -0,0 +1,31 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Leonardo Barbosa
+# Data: 03/09/2024
+# Faça um programa que peça 4 notas, após a entrada de dados calcular a média das notas digitadas.
+import os
+
+
+class Notas:
+    def __init__(self, nota1, nota2, nota3, nota4):
+        self.nota1 = nota1
+        self.nota2 = nota2
+        self.nota3 = nota3
+        self.nota4 = nota4
+        
+    def calcular_media(self, nota1, nota2, nota3, nota4):
+        # Usar try except para tratar erros
+        soma = float(nota1) + float(nota2) + float(nota3) + float(nota4)
+        media = soma / 4
+        return media
+
+
+os.system('cls')
+nota1 = input('Entre com a primeira nota: ')
+nota2 = input('Entre com a segunda nota.: ')
+nota3 = input('Entre com a terceira nota: ')
+nota4 = input('Entre com a quarta nota..: ')
+notas = Notas(nota1, nota2, nota3, nota4)
+resultado = notas.calcular_media(nota1, nota2, nota3, nota4)
+print(f'A média das notas {nota1}, {nota2}, {nota3}, {nota4} é: {resultado}')
+    
