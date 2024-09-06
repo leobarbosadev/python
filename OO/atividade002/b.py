c33571aae7c3c7982928f1bbc492ef9fcf7ccf32
leobarbosadev <leobarbosajf@hotmail.com>
2024-09-06 20:35:22 -0300

    finalizado exercicio b

--- OO/atividade002/b.py
+++ OO/atividade002/b.py
@@ -1,19 +1,27 @@
-#Evolua o programa anterior para um código que pergunte ao usuário qual o
+# Evolua o programa anterior para um código que pergunte ao usuário qual o
 # intervalo que ele deseja ver  impresso.
-
 import os
 
 
+class Intervalo:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+
+class ExibirIntervaloEntreNumeros(Intervalo):
+    def exibir(self):
+        for c in range(self.inicio, self.fim + 1):  # for c in range(0, 100)
+            print(f'{c}', end=' | ')  # print(f'{c + 1}', end=',')
+
+
 os.system('cls')
-
-print('-' * 80)
-print('*** CONTAR ENTRE INTERVALOS DEFINIDOS PELO USUÁRIO ***')
-print('=' * 80)
-print()
-
 inicio = int(input('Entre com um número inicial: '))
-fim = (int(input('Entre com um número final: '))) + 1
-
-for c in range(inicio, fim):
-        print(f'{c}', end= ', ')
+fim = (int(input('Entre com um número final: ')))
+print('-' * 80)
+print(f'*** DE 1 ATÉ {fim} ***')
+intervalo = ExibirIntervaloEntreNumeros(inicio, fim)
+exibir_intervalo = intervalo.exibir()
+print()
+print('=' * 80)
 print()
