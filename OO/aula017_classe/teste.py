850d3f2355e3813ec35fc83b94195b6403fe736d
leobarbosadev <leobarbosajf@hotmail.com>
2024-09-12 21:35:11 -0300

    mudado o nome do arquvo

--- OO/aula017_classe/teste.py
+++ OO/aula017_classe/teste.py
@@ -1,18 +1,43 @@
-class ClassePai:  # Super classe ou classe pai
-    def __init__(self, a, b):  # método construtor (a e b são atributos)
+class ClassePai:
+    def __init__(self, a, b):  # metodo construtor
+        # iniciar os atributos
         self.a = a
         self.b = b
 
-    def metodo_classe_pai(self):
-        print('Metodo da classe pai')
+
+class ClasseFilha(ClassePai):
+    def __init__(self, a, b):  # metodo construtor da classe pai
+        # iniciar os atributos
+        self.a = a
+        self.b = b
+
+    def metodo_classe_filha(self):
+        print('Subtracao')
+        subtracao = self.a - self.b
+        print(f'O resultado é: {subtracao}')
 
 
+# instaciando um objeto e passando valores para os atributos
+exibir = ClasseFilha(10, 2)
+exibir.metodo_classe_filha()
+
+
+class ClassePai: # Criacao da classe pai
+    def __init__(self, a, b): # metodo construtor
+        # iniciar os atributos
+        self.a = a
+        self.b = b
+        
 class ClasseFilha(ClassePai): # classe filha herda classe pai
-    def __init__(self, a, b): # metodo construtor da super classe
-        super().__init__(a, b) # acessa o método da classe pai
-
-    def metodo_classe_pai(self): # metodo da classe pai herdado para filha
-        return super().metodo_classe_pai()
-
-mostrar = ClasseFilha(1, 2) # instanciando o objeto
-mostrar.metodo_classe_pai() # executando o metodo da classe filha
+    def __init__(self, a, b): # metodo construtor da classe pai
+        # iniciar os atributos
+        self.a = a
+        self.b = b
+        
+    def metodo_filho(self): # metodo da classe filha
+        print('Soma')
+        soma = self.a + self.b
+        print(f'A some é {soma}')
+        
+objeto = ClasseFilha(10, 2) # instanciando o objeto e passando valores para os atributos
+objeto.metodo_filho() # executando o metodo da classe filha
