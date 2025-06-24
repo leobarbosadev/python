cfac7bf01daa8dcf2cfb3c54f104ce502c7caa3c
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-06-24 11:37:17 -0300

    Corrigindo um sinal no exercicio b

--- Procedural/atividade002/b.py
+++ Procedural/atividade002/b.py
@@ -36,7 +36,7 @@ if numero1 < numero2 and numero1 < numero3:
     menor = numero1
 elif numero2 < numero1 and numero2 < numero3:
     menor = numero2
-elif numero3 > numero1 and numero3 < numero2:
+elif numero3 < numero1 and numero3 < numero2:
     menor = numero3
 else:
     resposta = f'Os números são iguais!!!'
